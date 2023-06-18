0f8821627bc2181ebaf2f97ce65590c99530bedc
Martin Olague <molague23@hotmail.com>
2023-06-18 14:46:14 -0700

    Added routes, controller, error, and error handling, finally connected to DB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-	res.status(200).json({ message: 'Welcome to the support desk API' });
+  res.status(200).json({ message: 'Welcome to the support desk API' });
 });
 
-app.listen(PORT, () => console.log(`Server started on posr ${PORT}`));
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
