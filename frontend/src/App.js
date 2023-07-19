2afe1514e5b50c97e8a4309d59fa5038f119af28
Martin Olague <molague23@hotmail.com>
2023-07-18 21:34:43 -0700

    Added Route Guard and New Ticket Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,23 +6,28 @@ import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 
 import { Header } from './components/Header';
+import { NewTicket } from './pages/NewTicket';
+import { PrivateRoute } from './components/PrivateRoute';
 
 function App() {
-	return (
-		<>
-			<Router>
-				<div className='container'>
-					<Header />
-					<Routes>
-						<Route path='/' element={<Home />} />
-						<Route path='/login' element={<Login />} />
-						<Route path='/register' element={<Register />} />
-					</Routes>
-				</div>
-			</Router>
-			<ToastContainer />
-		</>
-	);
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/new-ticket" element={<PrivateRoute />}>
+              <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App;
