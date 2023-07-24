3d44f51461d888daa66adc8574f256ec246e9469
Martin Olague <molague23@hotmail.com>
2023-07-23 19:07:59 -0700

    Added Notes to Redux state and also services to project

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -10,14 +10,14 @@ const Ticket = require('../models/ticketModel');
 // @access Private
 const getTickets = asyncHandler(async (req, res) => {
   // Get user using the id in the JWT
-  const user = await User.findById(req.user.id);
+  const user = await User.findById(req?.user?.id);
 
   if (!user) {
     res.status(401);
     throw new Error('User not found');
   }
 
-  const tickets = await Ticket.find({ user: req.user.id });
+  const tickets = await Ticket.find({ user: req?.user?.id });
 
   res.status(200).json(tickets);
 });
